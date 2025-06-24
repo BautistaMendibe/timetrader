1d389cca9249ec3e16796f7d0f2a6d720f4d497c
Bautista Mendibe <bautistamendibe10@gmail.com>
2025-06-23 23:36:14 -0300

    Se agrega firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const TimeTraderApp());
-}
-
-class TimeTraderApp extends StatelessWidget {
-  const TimeTraderApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const App();
+  
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print('Firebase initialized successfully');
+  } catch (e) {
+    print('Firebase initialization error: $e');
   }
+  
+  runApp(const App());
 }
